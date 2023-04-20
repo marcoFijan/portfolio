327c5dd29f62a8664244f63c8d6b5f96be5da7a4
Marco <marcofijan@hotmail.com>
2023-04-20 03:13:53 +0200

    :sparkles: Content to portfolio + added contact

--- src/MainCanvas.js
+++ src/MainCanvas.js
@@ -6,6 +6,7 @@ import { useState, useRef, useEffect } from "react";
 import HeroSection from "./HeroSection.js";
 import AboutSection from "./AboutSection.js";
 import PortfolioSection from "./PortfolioSection.js";
+import ContactSection from "./ContactSection.js"
 import {
   ScrollControls,
   Scroll,
@@ -24,14 +25,19 @@ export default function MainCanvas({}) {
             camera={{ position: [0, 0, 7] }}
           >
             <ambientLight color={"white"} intensity={0.3} />
-            <ScrollControls pages={6} damping={0.1}>
+            <ScrollControls pages={10} damping={0.1}>
               <Environment blur={0} preset="warehouse" />
               <Scroll>
               </Scroll>
               <Scroll html>
-                <HeroSection/>
+                {/* Introduction animation [1 page] */}
+                <HeroSection/> 
+                {/* Information about me [1 page] */}
                 <AboutSection/>
+                {/* Overview with a couple of projects I am proud of [6 pages] */}
                 <PortfolioSection/>
+                {/* Contactpage with a form [1 page] */}
+                <ContactSection/>
               </Scroll>
             </ScrollControls>
           </Canvas>
