1f179a540d628550725fd32f1ae2f06c432adad7
Marco <marcofijan@hotmail.com>
2023-04-20 03:13:59 +0200

    :sparkles: Content to portfolio + added contact

---
+++ src/ContactSection.js
@@ -0,0 +1,13 @@
+export default function SkillsSection() {
+  return (
+    <div className="max-w-wrapper mx-auto w-full px-8 bg-bgColorLight">
+      <article className="py-10 h-screen box-border">
+          <h1 className="text-bgColorLight">Interesse? Vragen?...</h1>
+          <h2 className="">Neem gerust contact met me op!</h2>
+          <p className="py-8 text-bgColorLight">
+            Wat is uw naam?
+          </p>
+    </article>
+    </div>
+  );
+}
--- src/PortfolioSection.js
+++ src/PortfolioSection.js
@@ -1,14 +1,33 @@
 export default function PortfolioSection() {
   return (
-    <article className="w-full h-screen">
-      <div className="max-w-wrapper mx-auto w-full pt-10 px-8">
-
+    <div className="max-w-wrapper mx-auto w-full px-8">
+      <article className="py-10 h-screen box-border">
           <h1 className="text-bgColorLight">Portfolio</h1>
           <h2 className="">Mijn projecten</h2>
           <p className="py-8 text-bgColorLight">
             Bekijk hier mijn projecten
           </p>
-      </div>
     </article>
+    <article className="py-10 h-screen box-border text-right">
+      <h1 className="text-bgColorLight">GWOTY Award - YoungOnes</h1>
+      <h2 className="">Stage project met NextJS, Tailwind en Figma</h2>      
+    </article>
+    <article className="py-10 h-screen box-border text-left">
+      <h1 className="text-bgColorLight">GWOTY Award - YoungOnes</h1>
+      <h2 className="">Stage project met NextJS, Tailwind en Figma</h2>      
+    </article>
+    <article className="py-10 h-screen box-border text-right">
+      <h1 className="text-bgColorLight">GWOTY Award - YoungOnes</h1>
+      <h2 className="">Stage project met NextJS, Tailwind en Figma</h2>      
+    </article>
+    <article className="py-10 h-screen box-border text-left">
+      <h1 className="text-bgColorLight">GWOTY Award - YoungOnes</h1>
+      <h2 className="">Stage project met NextJS, Tailwind en Figma</h2>      
+    </article>
+    <article className="py-10 h-screen box-border text-right">
+      <h1 className="text-bgColorLight">GWOTY Award - YoungOnes</h1>
+      <h2 className="">Stage project met NextJS, Tailwind en Figma</h2>      
+    </article>
+    </div>
   );
 }
