3d97a3e49c276b93b7afef6a9e1416d54a7a7e6d
Marco <marcofijan@hotmail.com>
2023-04-20 02:31:10 +0200

    :sparkles: Added HeroSection

---
+++ src/MainCanvas.js
@@ -0,0 +1,39 @@
+"use client";
+
+// import Projects from "./PortfolioProjects.jsx";
+import { Canvas } from "@react-three/fiber";
+import { useState, useRef, useEffect } from "react";
+import HeroSection from "./HeroSection.js";
+import {
+  ScrollControls,
+  Scroll,
+  Environment,
+  Float,
+  useScroll,
+} from "@react-three/drei";
+
+export default function MainCanvas({}) {
+  return (  
+      <article className="bg-bgColorDark w-full h-screen">
+        <div className="w-full h-screen">
+          <Canvas
+            shadows
+            className="h-full w-full overflow-hidden"
+            camera={{ position: [0, 0, 7] }}
+          >
+            <ambientLight color={"white"} intensity={0.3} />
+            <ScrollControls pages={6} damping={0.1}>
+              <Environment blur={0} preset="warehouse" />
+              <Scroll>
+              </Scroll>
+              <Scroll html>
+                
+                <HeroSection/>
+              </Scroll>
+            </ScrollControls>
+          </Canvas>
+        </div>
+      </article>
+   
+  );
+}
