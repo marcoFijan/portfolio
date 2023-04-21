c1df7849761bd7767179bcdc28953e5a42891a95
Marco <marcofijan@hotmail.com>
2023-04-21 14:22:14 +0200

    :sparkles: Added Contactform functionalitiy

--- src/ContactSection.js
+++ src/ContactSection.js
@@ -1,13 +1,77 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 export default function SkillsSection() {
+  const form = useRef();
+  const [succes, setSuccess] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_s4iwgms",
+        "template_26bx8nk",
+        form.current,
+        "82Kmqh7RQh-GfNBXd"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSuccess(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setSuccess(false);
+        }
+      );
+  };
+
   return (
     <div className="max-w-wrapper mx-auto w-full px-8 bg-bgColorLight">
       <article className="py-10 h-screen box-border">
-          <h1 className="text-bgColorLight">Interesse? Vragen?...</h1>
-          <h2 className="">Neem gerust contact met me op!</h2>
-          <p className="py-8 text-bgColorLight">
-            Wat is uw naam?
-          </p>
-    </article>
+        <h1 className="">Interesse?</h1>
+        <h2 className="">Neem gerust contact met me op!</h2>
+        <p className="py-8 text-bgColorLight">Wat is uw naam?</p>
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          className="w-full h-full max-w-xl max-h-60 mx-auto flex flex-col"
+        >
+          <label htmlFor="name">Uw naam</label>
+          <input
+            type="name"
+            id="name"
+            name="name"
+            required
+            className="mb-8 rounded-md border border-bgColorDark p-2"
+          />
+          <label htmlFor="email">Uw mail</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            className="mb-8 rounded-md border border-bgColorDark p-2"
+          />
+          <label htmlFor="message">Uw bericht</label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            cols="50"
+            required
+            className="mb-8 rounded-md border border-bgColorDark p-2"
+          ></textarea>
+          <button
+            className="mb-8 text-bgColorLight font-bold bg-CTAColor p-2 rounded-full"
+            type="submit"
+          >
+            Verstuur bericht
+          </button>
+          {succes && "Uw bericht is succesvol verzonden!"}
+        </form>
+      </article>
     </div>
   );
 }
--- src/MainCanvas.js
+++ src/MainCanvas.js
@@ -7,7 +7,7 @@ import HeroSection from "./HeroSection.js";
 import AboutSection from "./AboutSection.js";
 import PortfolioSection from "./PortfolioSection.js";
 import PortfolioAnimation from "./PortfolioAnimation.js";
-import ContactSection from "./ContactSection.js"
+import ContactSection from "./ContactSection.js";
 import {
   ScrollControls,
   Scroll,
@@ -17,34 +17,32 @@ import {
 } from "@react-three/drei";
 
 export default function MainCanvas({}) {
-  return (  
-      <article className="bg-bgColorDark w-full h-screen">
-        <div className="w-full h-screen">
-          <Canvas
-            shadows
-            className="h-full w-full overflow-hidden"
-            camera={{ position: [0, 0, 7] }}
-          >
-            <ambientLight color={"white"} intensity={0.3} />
-            <ScrollControls pages={10} damping={0.1}>
-              <Environment blur={0} preset="warehouse" />
-              <PortfolioAnimation/>
-              <Scroll>
-              </Scroll>
-              <Scroll html>
-                {/* Introduction animation [1 page] */}
-                <HeroSection/> 
-                {/* Information about me [1 page] */}
-                <AboutSection/>
-                {/* Overview with a couple of projects I am proud of [6 pages] */}
-                <PortfolioSection/>
-                {/* Contactpage with a form [1 page] */}
-                <ContactSection/>
-              </Scroll>
-            </ScrollControls>
-          </Canvas>
-        </div>
-      </article>
-   
+  return (
+    <article className="bg-bgColorDark w-full h-screen">
+      <div className="w-full h-screen">
+        <Canvas
+          shadows
+          className="h-full w-full overflow-hidden"
+          camera={{ position: [0, 0, 7] }}
+        >
+          <ambientLight color={"white"} intensity={0.3} />
+          <ScrollControls pages={10} damping={0}>
+            <Environment blur={0} preset="warehouse" />
+            <PortfolioAnimation />
+            <Scroll></Scroll>
+            <Scroll html>
+              {/* Introduction animation [1 page] */}
+              <HeroSection />
+              {/* Information about me [1 page] */}
+              <AboutSection />
+              {/* Overview with a couple of projects I am proud of [6 pages] */}
+              <PortfolioSection />
+              {/* Contactpage with a form [1 page] */}
+              <ContactSection />
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+      </div>
+    </article>
   );
 }
