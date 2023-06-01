aceebc3e3a29d7ca16d4e0246fb164d501510970
Marco <marcofijan@hotmail.com>
2023-06-01 16:23:08 +0200

    :sparkles: Optimalisation for mobile

--- src/organisms/PortfolioSection.js
+++ src/organisms/PortfolioSection.js
@@ -10,14 +10,14 @@ export default function PortfolioSection() {
         darkBackground={darkBackground}
         title={"Portfolio"}
         subTitle={"Mijn projecten"}
-        paragraph1={
-          "De afgelopen jaren heb ik verschillende diverse projecten ondernomen. De projecten verschillen van UX/UI-design tot daadwerkelijke frontend. Hieronder vind u enkele projecten"
+        paragraph1a={
+          "De afgelopen jaren heb ik verschillende diverse projecten ondernomen. De projecten verschillen van UX/UI-design tot daadwerkelijke frontend. Hieronder vind u enkele projecten."
         }
       ></Section>
       <Section
         customClassName={"h-screen text-right"}
         darkBackground={darkBackground}
-        header={"KBO Tilburg Reeshof"}
+        header={"KBO Tilburg Reeshof - 2022"}
         subHeader={"Redesign website met de focus voor ouderen binnen budget"}
         linkHref={"/projects/kbo-tilburg-reeshof"}
         linkText={"Bekijk het project"}
@@ -25,23 +25,23 @@ export default function PortfolioSection() {
       <Section
         customClassName={"h-screen text-left"}
         darkBackground={darkBackground}
-        header={"GWOTY Award - YoungOnes"}
-        subHeader={"Stage project met NextJS, Tailwind en Figma"}
+        header={"Dashboard YoungOnes - 2022"}
+        subHeader={"Stage project met NextJS, Tailwind Figma en ChartJS"}
         linkHref={"/projects/gwoty-award-youngones"}
         linkText={"Bekijk het project"}
       ></Section>
       <Section
         customClassName={"h-screen text-right"}
         darkBackground={darkBackground}
-        header={"Dashboard - YoungOnes"}
-        subHeader={"Stage project met NextJS, Tailwind Figma en ChartJS"}
+        header={"GWOTY Award YoungOnes - 2021"}
+        subHeader={"Stage project met NextJS, Tailwind en Figma"}
         linkHref={"/projects/dashboard-youngones"}
         linkText={"Bekijk het project"}
       ></Section>
       <Section
         customClassName={"h-screen text-left"}
         darkBackground={darkBackground}
-        header={"Standaard Aanbouw"}
+        header={"Standaard Aanbouw - 2020"}
         subHeader={"Commercieel Wordpress website voor een aannemer"}
         linkHref={"/projects/standaard-aanbouw"}
         linkText={"Bekijk het project"}
@@ -53,7 +53,7 @@ export default function PortfolioSection() {
         subHeader={
           "Andere projecten uit diverse achtergronden waaronder Unity-engine, Blender, pure CSS"
         }
-        linkHref={"/projects/standaard-aanbouw"}
+        linkHref={"/projects"}
         linkText={"Bekijk het project"}
       ></Section>
     </article>
