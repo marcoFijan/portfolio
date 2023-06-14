f0eef79855058b4682a0e2e49d72ac1c4df648c8
Marco <marcofijan@hotmail.com>
2023-06-14 20:16:44 +0200

    :sparkles: Added meta tags

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,4 +1,5 @@
 import { Montserrat } from "next/font/google";
+import Head from "next/head";
 
 import ContactSection from "@/organisms/ContactSection.js";
 import Header from "@/organisms/Header.js";
@@ -13,6 +14,18 @@ const montserrat = Montserrat({
 export default function Contact() {
   return (
     <>
+      <Head>
+        <title>Marco Fijan - Contact</title>
+        <meta
+          name="description"
+          content="Contact a Frontend Developer, Marco Fijan"
+        />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, NextJS, React, Svelte, Unity, Front, End, Frontend, Developer, Development, Git, Github, Portfolio, NL, Soliciting, Job, Vacature, Baan, UI, User, Interface, UX, Experience, Contact"
+        />
+        <meta name="author" content="Marco Fijan" />
+      </Head>
       <Header />
       <main className={`bg-bgColorDark pb-[50vh] ${montserrat.className}`}>
         <ContactSection />
